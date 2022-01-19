8cc58b1473f9ca530bd69166054421247c8835f2
Vlad <vlad.herasymovych@tutore.eu>
2022-01-19 16:46:13 +0100

    basic forms validation

--- src/pages/Login.vue
+++ src/pages/Login.vue
@@ -5,22 +5,22 @@
                 <v-card class="white--text" shaped elevation="10">
                     <v-card-title class="deep-purple lighten-2" primary-title>{{ title }}</v-card-title>
                     <v-card-text>
-                        <v-form v-if="isLogin" class="mt-10">
-                            <v-text-field label="Login" v-model="login" prepend-icon="badge"></v-text-field>
-                            <v-text-field label="Hasło" type="password" v-model="password" prepend-icon="password"></v-text-field>
+                        <v-form v-if="isLogin" class="mt-10" ref="form">
+                            <v-text-field label="Login" prepend-icon="badge" v-model="loginInput" :rules="loginRule"></v-text-field>
+                            <v-text-field label="Hasło" type="password" prepend-icon="password"  v-model="passwordInput" :rules="passwordRule"></v-text-field>
                             <div class="d-flex justify-space-between">
                                 <v-btn @click="loginBtn" dark large class="ma-7" color="deep-purple lighten-2">Login</v-btn>
                                 <v-btn @click.prevent="changeForm" large outlined class="ma-7" color="deep-purple lighten-2">SignUp</v-btn>
                             </div>
                         </v-form>
-                        <v-form v-if="!isLogin" class="mt-10">
+                        <v-form v-if="!isLogin" class="mt-10" ref="form">
                             <h2 class="display-1">Wypełnij formularz</h2>
                             <div class="mt-10">
-                                <v-text-field label="Imię" v-model="login" type="text" prepend-icon="badge"></v-text-field>
-                                <v-text-field label="Nazwisko" v-model="password" type="text" prepend-icon="badge"></v-text-field>
-                                <v-text-field label="E-mail" v-model="password" type="email" prepend-icon="email"></v-text-field>
-                                <v-text-field label="Telefon" v-model="password" type="text" prepend-icon="phone"></v-text-field>
-                                <v-text-field label="Hasło" type="password" v-model="password" prepend-icon="password"></v-text-field>
+                                <v-text-field label="Imię" type="text" v-model="nameInput" prepend-icon="badge" :rules="nameRule"></v-text-field>
+                                <v-text-field label="Nazwisko" type="text" v-model="surnameInput" prepend-icon="badge" :rules="nameRule"></v-text-field>
+                                <v-text-field label="E-mail" type="email" v-model="emailInput" prepend-icon="email" :rules="loginRule"></v-text-field>
+                                <v-text-field label="Telefon" type="text" v-model="phoneInput" prepend-icon="phone" :rules="phoneRule"></v-text-field>
+                                <v-text-field label="Hasło" type="password" v-model="passwordInput" prepend-icon="password" :rules="passwordRule"></v-text-field>
                             </div>
                             <div class="d-flex justify-space-between">
                                 <v-btn @click="signupBtn" large dark class="ma-7" color="deep-purple lighten-2">SignUp</v-btn>
@@ -39,7 +39,29 @@ export default {
     data() {
         return {
             isLogin: true,
-            title: 'Zaloguj się'
+            title: 'Zaloguj się',
+            loginInput: '',
+            passwordInput: '',
+            nameInput: '',
+            surnameInput: '',
+            emailInput: '',
+            phoneInput: '',
+            loginRule: [
+                login => login.length >= 5 || 'Minimum length is 5 characters'
+            ],
+            // passwordRule: [
+            //     password => password.length >= 6 || 'Minimum length is 6 characters'
+            // ],
+            nameRule: [
+                name => name.length >= 2 || "Minimum length is 2 characters"
+            ],
+            passwordRule: [
+                password => password.length >= 1 || 'Enter min. 6 chars and must include numbers and special chars (!@#$%^&*())'
+                // && this.passwordInput.value.includes(/[!@#$%^&*()01234567890]/gi)
+            ],
+            phoneRule: [
+                phone => phone.length >= 6 || 'Enter phone number'
+            ]
         };
     },
     methods: {
@@ -52,10 +74,14 @@ export default {
             }
         },
         loginBtn() {
-
+            if(this.$refs.form.validate()) {
+                console.log(this.title + ' ' + this.isLogin);
+            }
         },
         signupBtn() {
-
+            if(this.$refs.form.validate()) {
+                console.log(this.title + ' ' + this.isLogin);
+            }
         }
     }
 }
